def6e95b83caf234dceb06fd887a6c6bb0837587
nifist99 <nikofigit@gmail.com>
2022-08-13 22:07:26 +0700

    manage link

--- resources/views/web/tema/content.blade.php
+++ resources/views/web/tema/content.blade.php
@@ -15,6 +15,16 @@
     @include('web.tema.css')
 
     @stack('css')
+
+            <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8B8Z8NW0ZR"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-8B8Z8NW0ZR');
+        </script>
 </head>
 <body>
     <!-- Page Preloder -->
